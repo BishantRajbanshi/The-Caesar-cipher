19ac66884e3d61e54809207a05e0d14c025a914d
BishantRajbanshi <rajbancbishant@gmail.com>
2024-01-18 00:03:24 +0545

    Lets get started

--- caesar_0000.py
+++ caesar_0000.py
@@ -3,7 +3,15 @@
 def welcome():
   print("Welcome to the Ceasar Ciper\nThis program encrypts and decrypts text with the Ceasar Ciper")
 
-
-
+def message():
+  while True:
+    select = input("Would you like to encrypt (e) or dycrypt (d): ")
+    if select == 'e' or select == 'd':
+      messages= input('What message would you like to encrypt: ')
+      return select,messages
+    else:
+      print('Invalid Mode')
 
 welcome()
+mode, user_message = message()
+
